8727f79075b885eb5714fbcdf0198d9e775fe99a
Behnam <behnam.sep@gmail.com>
2024-12-26 17:41:55 +0100

    add final details

--- README.md
+++ README.md
@@ -1,43 +1,48 @@
 # Online-Reserve-App
+
 # Overview
+
 The Little Lemon Restaurant Booking App is a web application built using React. It allows customers to easily book a table at the Little Lemon restaurant by filling out a form. The app includes form validation, user-friendly UI/UX, and accessibility features to ensure an inclusive experience for all users.
 
 # Features
+
 Table Booking Form: Allows users to book a table by entering details such as name, date, time, and number of people.
 Form Validation: Ensures that all required fields are completed correctly before submitting.
 Responsive Design: The app is optimized for various devices, ensuring a smooth experience on desktops, tablets, and smartphones.
 Accessibility: Proper use of semantic HTML and accessibility tags to enhance usability for people with disabilities.
 Unit Tests: Unit tests are included to validate the functionality of the app.
+
 # Technologies Used
+
 React – JavaScript library for building user interfaces.
 CSS – For styling the application.
 HTML – To structure the web page.
 Jest – For unit testing.
+
 # Installation
+
 Clone this repository to your local machine.
 
-
-git clone https://github.com/yourusername/little-lemon-booking-app.git
 Navigate to the project folder.
-
 
 cd little-lemon-booking-app
 Install the required dependencies.
 
-
 npm install
 Start the development server.
 
-
-npm start
+npm run dev
 The app should now be running at http://localhost:3000.
 
 # Usage
+
 Once the app is running, users can:
 
 Fill out the table booking form with their name, the date and time for the booking, and the number of people.
 Submit the form to confirm their booking. The app will validate the inputs and show error messages if any required fields are missing or incorrect.
+
 # Testing
+
 Run the following command to execute the unit tests:
 
 npm test
@@ -45,4 +50,5 @@ Contributing
 If you'd like to contribute to this project, feel free to fork this repository and submit a pull request with your improvements. Please ensure all contributions follow the project’s coding standards and include relevant tests.
 
 # License
+
 This project is licensed under the MIT License - see the LICENSE file for details.
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,111 @@
+import { FC, HTMLAttributes } from "react";
+import { HashLink } from "react-router-hash-link";
+import { Container, Content, Copyright, Column, Image } from "./styles";
+import restaurant from "../../assets/restaurant.jpg";
+
+const Footer: FC<HTMLAttributes<HTMLElement>> = (props): JSX.Element => {
+  return (
+    <Container {...props}>
+      <Content>
+        <Image src={restaurant} alt="Restaurant Food" />
+        <Column>
+          <h4>
+            Little
+            <br />
+            Lemon
+          </h4>
+          <ul>
+            <li>
+              {" "}
+              <HashLink to="/">Home</HashLink>{" "}
+            </li>
+            <li>
+              {" "}
+              <HashLink to="/#">Menu</HashLink>{" "}
+            </li>
+            <li>
+              {" "}
+              <HashLink to="/bookings">Reservations</HashLink>{" "}
+            </li>
+            <li>
+              {" "}
+              <HashLink to="/#">Order Online</HashLink>{" "}
+            </li>
+            <li>
+              {" "}
+              <HashLink to="/#">Login</HashLink>{" "}
+            </li>
+          </ul>
+        </Column>
+        <Column>
+          <h4>Contact</h4>
+          <ul>
+            <li>
+              <address>
+                Little Lemon <br />
+                123 A Detroit <br />
+                25 street,
+                <br />
+                Detroit 12345 <br />
+                USA
+              </address>
+            </li>
+            <li>
+              {" "}
+              <a href="tel:+123456789" target="_blank" rel="external">
+                +1 99 123-546
+              </a>{" "}
+            </li>
+            <li>
+              {" "}
+              <a
+                href="mailto:contact@littlelemon.com"
+                target="_blank"
+                rel="external"
+              >
+                contact@littlelemon.com
+              </a>{" "}
+            </li>
+          </ul>
+        </Column>
+        <Column>
+          <h4>Social Media</h4>
+          <ul>
+            <li>
+              {" "}
+              <a
+                href="https://www.facebook.com/"
+                target="_blank"
+                rel="external"
+              >
+                Facebook
+              </a>{" "}
+            </li>
+            <li>
+              {" "}
+              <a
+                href="https://www.instagram.com/"
+                target="_blank"
+                rel="external"
+              >
+                Instagram
+              </a>{" "}
+            </li>
+            <li>
+              {" "}
+              <a href="https://twitter.com/" target="_blank" rel="external">
+                Twitter
+              </a>{" "}
+            </li>
+          </ul>
+        </Column>
+      </Content>
+      <Copyright>
+        <span>Developed by Behnam.</span>
+        <p>© 2024 Little Lemon. All rights reserved.</p>
+      </Copyright>
+    </Container>
+  );
+};
+
+export default Footer;
